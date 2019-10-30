baec954d2cc1fad83792aaf0b388cb34833c7154
mmb L <mammothb+git@gmail.com>
2019-10-30 11:28:56 +0800

    added save and load funciton

--- chess/lib/chess.rb
+++ chess/lib/chess.rb
@@ -1,18 +1,27 @@
 # frozen_string_literal: true
 
+require 'fileutils'
+require 'yaml'
+
 require_relative 'board.rb'
 require_relative 'computer.rb'
 require_relative 'player.rb'
 
 # Main class to control the flow of a chess game
 class Chess
-  attr_reader :board
+  DATA_DIR = File.join(File.dirname(__dir__), 'data')
+  PERMITTED_CLASSES = [Bishop, Board, BoardValues, Chess, Computer, King,
+                       Knight, Pawn, Player, Queen, Rook, Symbol].freeze
+
+  attr_accessor :board, :players
+  attr_writer :current_player
 
   def initialize(player_one, player_two)
     @players = [player_one, player_two]
     @board = Board.new
     @board.game = self
     @current_player = 0
+    FileUtils.mkdir_p(DATA_DIR)
   end
 
   def current_player
@@ -28,15 +37,22 @@ class Chess
     switch_player
   end
 
-  def play
-    puts 'Flip a coin to decide who starts first' unless prepared?
-    until game_over?
-      turn_message
-      move = request_player_move
-      board.execute_move(move)
-      switch_player
+  def start
+    puts 'Would you like to:'
+    puts '1. Start new game'
+    puts '2. Load game'
+    choice = gets.chomp
+    until /^\d$/ =~ choice && choice.to_i.between?(1, 2)
+      print 'Please choose only between 1 and 2: '
+      choice = gets.chomp
     end
-    game_over_message
+    case choice.to_i
+    when 1
+      play
+    when 2
+      load_game
+      play
+    end
   end
 
   def request_pawn_promotion
@@ -49,9 +65,24 @@ class Chess
     @current_player = (@current_player + 1) % 2
   end
 
+  def play
+    puts 'Flip a coin to decide who starts first' unless prepared?
+    until game_over?
+      turn_message
+      move = request_player_move
+      if save_game?(move)
+        save_game
+        break
+      end
+      board.execute_move(move)
+      switch_player
+    end
+    game_over_message unless save_game?(move)
+  end
+
   def request_player_move
     move = convert_to_coordinate(current_player.make_move)
-    until board.valid_move?(move, current_player.color)
+    until save_game?(move) || board.valid_move?(move, current_player.color)
       move = convert_to_coordinate(current_player.make_move)
     end
     move
@@ -73,6 +104,39 @@ class Chess
     end
   end
 
+  def save_game
+    print 'Enter a name for the save file: '
+    filename = gets.chomp
+    until valid_filename?(filename)
+      print 'Enter a valid filename: '
+      filename = gets.chomp
+    end
+    File.open(File.join(DATA_DIR, filename), 'w') do |file|
+      file.write(YAML.dump(self))
+    end
+    puts 'Game saved!'
+  end
+
+  def load_game
+    Dir[File.join(DATA_DIR, '*')].each do |filename|
+      puts File.basename(filename)
+    end
+    print 'Which save file would you like to load?: '
+    filename = gets.chomp
+    until File.exist?(File.join(DATA_DIR, filename))
+      print 'Enter a valid filename: '
+      filename = gets.chomp
+    end
+    File.open(File.join(DATA_DIR, filename), 'r') do |object|
+      tmp = YAML.safe_load(object, permitted_classes: PERMITTED_CLASSES,
+                                   aliases: true)
+      @players = tmp.players
+      @board = tmp.board
+      @board.game = tmp
+      @current_player = @players.index(tmp.current_player)
+    end
+  end
+
   def prepared?
     @players.all?(&:color)
   end
@@ -81,9 +145,24 @@ class Chess
     !board.escape_checked?(current_player.color)
   end
 
+  def save_game?(move)
+    move == 'save'
+  end
+
+  def valid_filename?(filename)
+    begin
+      File.open(File.join(DATA_DIR, filename), 'a') {}
+      true
+    rescue Errno::ENOENT
+      false
+    end
+  end
+
   # Convert move string [col, row] to matrix coordinates [row, col],
   # e.g., a2 c5 => { from: [1, 0], to: [4, 2] }
   def convert_to_coordinate(move_string)
+    return move_string if move_string == 'save'
+
     move_array = move_string.split(' ')
     return convert_castling(move_string.size == 3) if move_array.size == 1
 
@@ -108,12 +187,7 @@ if $PROGRAM_NAME == __FILE__
 
   game = Chess.new(p1, p2)
   p2.board = game.board
-  # 8.times { |i| 8.times { |j| game.board[i][j] = Board::EMPTY } }
-
-  # game.board.place(King.new(:b, [7, 4]))
-  # game.board.place(Pawn.new(:w, [6, 4]))
-  # game.board.place(King.new(:w, [4, 4]))
 
   game.flip_coin
-  game.play
+  game.start
 end
--- chess/lib/player.rb
+++ chess/lib/player.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# Handles player input for moves and pawn promotion
 class Player
   attr_reader :name, :color, :icon
 
@@ -16,21 +17,26 @@ class Player
 
   def make_move
     move = gets.chomp
-    until /^[a-z]\d\s[a-z]\d$/ =~ move || ['0-0', '0-0-0'].include?(move)
+    until save_game(move) || /^[a-z]\d\s[a-z]\d$/ =~ move ||
+          ['0-0', '0-0-0'].include?(move)
       print 'Please use proper move notation ([a..h][1..8] [a..h][1..8]): '
       move = gets.chomp
     end
-    move
+    save_game(move) ? move.downcase : move
   end
 
   def promote_pawn
     print 'Please choose a promotion: 1) Queen, 2) Knight, 3) Rook, ' \
           '4) Bishop: '
     choice = gets.chomp
-    until /^\d$/ =~ choice && choice.to_i.between?(1, 4)
+    until save_game(move) || (/^\d$/ =~ choice && choice.to_i.between?(1, 4))
       print 'Please choose only between 1 to 4: '
       choice = gets.chomp
     end
     choice.to_i
+  end
+
+  def save_game(move)
+    /^save$/i =~ move
   end
 end
